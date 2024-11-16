c48685a21f04ffa4c66cab5fa044126b7531ae2c
ChanAlex2357 <jacqueschanalex@gmail.com>
2024-11-16 21:39:52 +0300

    restucturation du frontend

---
+++ frontend/src/components/FormulaireSimple.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+      <h1>Gestion des paiements</h1>
+      <PaymentForm @submit-payment="handlePayment" />
+    </div>
+  </template>
+  
+  <script>
+  import PaymentForm from "./PaymentForm.vue";
+  
+  export default {
+    components: {
+      PaymentForm,
+    },
+    methods: {
+      handlePayment(paymentData) {
+        console.log("Données de paiement reçues :", paymentData);
+  
+        // Envoyer les données à une API ou les traiter
+        // Exemple : utiliser fetch ou axios
+        // axios.post('/scoot-api/payement', paymentData).then(...);
+      },
+    },
+  };
+  </script>
+  
